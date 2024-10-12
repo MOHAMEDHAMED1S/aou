c926cfe0243668914eda5d544dbd02f9108568ba
Mohamed Hamed <mmop9909@gmail.com>
2024-10-12 04:08:53 +0300

    .

--- scripts.js
+++ scripts.js
@@ -5,3 +5,26 @@ window.addEventListener("load", function() {
         document.getElementById("content").style.display = "block";
     }, 600); // مدة التأخير 1 ثانية
 });
+    const checkbox = document.querySelector('.input');
+    const elements = document.querySelectorAll('.button, .card, .card i, .card h3, h1, header, .cardd, .container, .loader-wrapper, .circle, .logo-link');
+
+    const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+
+    checkbox.checked = darkModeEnabled;
+
+    if (darkModeEnabled) {
+        document.body.classList.add('dark-mode');
+        elements.forEach(element => {
+            element.classList.add('dark-mode');
+        });
+    }
+
+    checkbox.addEventListener('change', () => {
+        const isChecked = checkbox.checked;
+        document.body.classList.toggle('dark-mode', isChecked);
+        elements.forEach(element => {
+            element.classList.toggle('dark-mode', isChecked);
+        });
+
+        localStorage.setItem('darkMode', isChecked);
+    });
