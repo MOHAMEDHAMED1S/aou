cef02c19097043a084e55e84933471cfdc415fd0
Mohamed Hamed <163942536+MOHAMEDHAMED1S@users.noreply.github.com>
2024-12-23 23:08:38 +0200

    .

--- g.js
+++ g.js
@@ -5,7 +5,7 @@ function doGet(e) {
   function doPost(e) {
     return handleRequest(e);
   }
-  var folderr = "1bybfRm1gGpgSopScFTlxe9H6KF8L9Vi5"
+  var folderr = ""
   function handleRequest(e) {
     if (e.method === "OPTIONS") {
       return HtmlService.createHtmlOutput("")
